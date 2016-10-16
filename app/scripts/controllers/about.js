0b0eb1961859b948f31eb29ffd714c529d8c4343
PaulExina <paulexina26@gmail.com>
2016-10-17 00:41:20 +0200

    Setting up project...

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name secretaryApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the secretaryApp
+ */
+angular.module('secretaryApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name secretaryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the secretaryApp
+ */
+angular.module('secretaryApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
